c2aee49ef571247c1936e9a832a73d6425c14729
rsandell <sandell.robert@gmail.com>
2013-01-16 22:48:44 +0100

    Status controller

--- grails-app/controllers/statist/StatusController.groovy
+++ grails-app/controllers/statist/StatusController.groovy
@@ -1,9 +1,13 @@
 package statist
 
+import com.sonymobile.swdtools.statist.StatusItem
+import com.sonymobile.swdtools.statist.System
+
 class StatusController {
 
     def status = {
-
+        [systems: System.list(sort: "prio", order: "desc"),
+         status: StatusItem.list(offset: params.offset, max: 25, order: "date")]
     }
 
     def index() { redirect( action:status ) }
