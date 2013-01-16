29b64dab73e32e42cc99a6996f640f8b05ffd8b6
Fredrik Luthander <frazor@gmail.com>
2013-01-16 20:47:36 +0100

    Prepopulating items via Bootstrap
    This is a temporary solution until we have a persistent item store in place.
    Will be reverted once such a store is in place.
    
    Change-Id: I3994cda2f7d82bd7f2f58eb062e3c78f595787fc
    Signed-off-by: Fredrik Luthander <frazor@gmail.com>

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,40 @@
+import com.sonymobile.swdtools.statist.*
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+		switch (Environment.current) {
+			case Environment.DEVELOPMENT:
+				def Gerrit = new System(name: "Gerrit", prio: 1, archived: 0).save()
+				def Jenkins = new System(name: "Jenkins", prio: 1, archived: 0).save()
+				def CrashTool = new System(name: "CrashTool", prio: 3, archived: 0).save()
+				def ClearCase = new System(name: "ClearCase", prio: 1, archived: 1).save()
+				def TagGerrit = new Tag(name: "GERRIT").save()
+				def TagJenkins = new Tag(name: "JENKINS").save()
+				def TagSeld = new Tag(name: "SELD").save()
+				def TagJpto = new Tag(name: "JPTO").save()
+				def TagCnbj = new Tag(name: "CNBJ").save()
+				def StatusOk = new Status(name: "Ok", severity: 10).save()
+				def Status1 = new Status (name: "1", severity: 1).save()
+				def Status2 = new Status (name: "2", severity: 2).save()
+				def Status3 = new Status (name: "3", severity: 3).save()
+				def Status4 = new Status (name: "4", severity: 4).save()
+				def Status5 = new Status (name: "5", severity: 5).save()
+				def Status01 = new StatusItem(system: Gerrit, text: "Replication issue 01", tag: TagSeld, date: new Date(), status: Status4).save()
+				def Status02 = new StatusItem(system: Gerrit, text: "Replication issue 02", tag: TagSeld, date: new Date(), status: Status4).save()
+				def Status03 = new StatusItem(system: Gerrit, text: "Replication issue 03", tag: TagJpto, date: new Date(), status: Status4).save()
+				def Status04 = new StatusItem(system: Gerrit, text: "Replication issue 04", tag: TagJpto, date: new Date(), status: Status4).save()
+				def Status05 = new StatusItem(system: Jenkins, text: "Build issue 05", tag: TagSeld, date: new Date(), status: Status5).save()
+				def Status06 = new StatusItem(system: Jenkins, text: "Uptime issue 06", tag: TagJpto, date: new Date(), status: Status2).save()
+				def Status07 = new StatusItem(system: Jenkins, text: "Uptime issue 07", tag: TagCnbj, date: new Date(), status: Status1).save()
+				def Status08 = new StatusItem(system: Gerrit, text: "Replication issue 08", tag: TagSeld, date: new Date(), status: Status3).save()
+
+				break
+			case Environment.PRODUCTION:
+				break
+       }
+
     }
     def destroy = {
     }
